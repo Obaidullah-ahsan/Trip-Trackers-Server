1d082a76bf0fd3e63859766f6a39fd5ecedda5cb
Obaidullah-ahsan <abir60303@gmail.com>
2024-04-28 22:03:21 +0600

    added mongodb post

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // Middleware
 app.use(cors());
-app.use(express());
-
+app.use(express.json()); 
 
 const uri = `mongodb+srv://${process.env.TRIP_USER}:${process.env.TRIP_PASS}@cluster0.zkk0rbw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,23 +17,35 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const touristSpotCollection = client
+      .db("touristSpotDB")
+      .collection("touristSpot");
+
+    app.post("/touristspot", async (req, res) => {
+      const touristSpot =req.body;
+      const result = await touristSpotCollection.insertOne(touristSpot);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Tourism Management website server running!");
